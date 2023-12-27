7b23d5985dec84a419edfbaaa3ab5a1aaa61276c
césar <cesaralonso99@gmail.com>
2023-12-27 16:03:16 -0300

    url

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = 'mongodb+srv://cesaralonso:cesaralonso@cluster0.nvi0fy7.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = '';
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
